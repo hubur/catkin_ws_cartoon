3dc9c1dc066b3f1eb3c3c0aa16ccc4bbc532a24d
mhocke <mathis.hocke@hubur.org>
2019-08-13 21:50:26 +0200

    Use NormalizedSteeringCommand

--- src/assignment2_publisher_subscriber/src/publisher.py
+++ src/assignment2_publisher_subscriber/src/publisher.py
@@ -1,16 +1,17 @@
 #!/usr/bin/env python
+import math
 import rospy
-from autominy_msgs.msg import SteeringCommand, SpeedCommand
+from autominy_msgs.msg import NormalizedSteeringCommand, SpeedCommand
 from std_msgs.msg import Header
 from rospy.timer import Rate
 
 rospy.init_node("basic_pub")
 
-steering_publisher = rospy.Publisher("/actuators/steering", SteeringCommand, queue_size=1)
+steering_publisher = rospy.Publisher("/actuators/steering_normalized", NormalizedSteeringCommand, queue_size=1)
 speed_publisher = rospy.Publisher("/actuators/speed/", SpeedCommand, queue_size=1)
 
-rate = Rate(hz=100)
+rate = Rate(hz=100, reset=True)
 while not rospy.is_shutdown():
-    steering_publisher.publish(Header(), 0.0)
+    steering_publisher.publish(Header(), 1.0)
     speed_publisher.publish(Header(), 0.3)
     rate.sleep()
