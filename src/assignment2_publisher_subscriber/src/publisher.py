1e2599e2c8fa250b1e3425b1691ea6b3fcd6960a
mhocke <mathis.hocke@hubur.org>
2019-08-13 17:19:13 +0200

    Fix direction to turn left

--- src/assignment2_publisher_subscriber/src/publisher.py
+++ src/assignment2_publisher_subscriber/src/publisher.py
@@ -2,13 +2,15 @@
 import rospy
 from autominy_msgs.msg import SteeringCommand, SpeedCommand
 from std_msgs.msg import Header
+from rospy.timer import Rate
 
 rospy.init_node("basic_pub")
 
 steering_publisher = rospy.Publisher("/actuators/steering", SteeringCommand)
 speed_publisher = rospy.Publisher("/actuators/speed/", SpeedCommand)
 
+rate = Rate(hz=100)
 while not rospy.is_shutdown():
-    steering_publisher.publish(Header(), 1.0)
+    steering_publisher.publish(Header(), 0.0)
     speed_publisher.publish(Header(), 0.3)
-    rospy.sleep(0.5)
+    rate.sleep()
