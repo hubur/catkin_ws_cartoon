0a9d7cbf8c8bb52aa0e3389f61a5d16269e2a8c8
mhocke <mathis.hocke@hubur.org>
2019-08-21 15:13:03 +0200

    Implement PID

--- src/u07/u07.py
+++ src/u07/u07.py
@@ -24,34 +24,48 @@ class GPSData(object):
         self.yaw = yaw
 
 
-def rotation_speed():
-    g1 = buffer[-2]
-    g2 = buffer[-1]
-    dsec = g2.sec - g1.sec
-    dnsec = g2.nsec - g1.nsec
-    dt = 1000000000 * dsec + dnsec
-    dyaw = g2.yaw - g1.yaw
-    return dyaw / dt
+def rotation_speed_diff():
+    def speed(i):
+        g1 = buffer[-2 - i]
+        g2 = buffer[-1 - i]
+        dsec = g2.secs - g1.secs
+        dnsec = g2.nsecs - g1.nsecs
+        dt = 1000000000 * dsec + dnsec
+        dyaw = g2.yaw - g1.yaw
+        return dyaw / dt
+
+    return speed(1) - speed(0)
+
+
+def integrate_error():
+    return sum([(p1.yaw - p2.yaw) for p1, p2 in zip(buffer[:-1], buffer[1:])])
 
 
 def steer(target_yaw):
     current = buffer[-1]
     d = target_yaw - current.yaw
-    steering_publisher.publish(Header(), 2 * d / math.pi)
+    k_p = 16 / math.pi
+    k_d = 10 ** 8
+    k_i = 50 / len(buffer)
+    rsd = rotation_speed_diff()
+    ie = integrate_error()
+    u = k_p * d + k_d * rsd + k_i * ie
+    print(d, rsd, ie)
+    steering_publisher.publish(Header(), u)
 
 
 def gps_callback(gps_msg):
     gps_data = GPSData(gps_msg)
     position = gps_data.position
-    if position.x < 0 or position.x > 5.5 or position.y < 0.5 or position.y > 4.2:
-        speed_publisher.publish(Header(), 0)
-    else:
-        speed_publisher.publish(Header(), 0.5)
     global buffer
     buffer.append(gps_data)
     if len(buffer) > BUFFER_SIZE:
         buffer = buffer[1:]
-    steer(0)
+    if position.x < 0 or position.x > 5.5 or position.y < 0.5 or position.y > 4.2:
+        speed_publisher.publish(Header(), 0)
+    else:
+        steer(0)
+        speed_publisher.publish(Header(), 0.5)
 
 
 rospy.init_node("gps_node")
